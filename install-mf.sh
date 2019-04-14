fd946eed50341e752d4d231dead3742d52e7b16c
Aishou <kaito.linux@protonmail.com>
2019-04-14 20:57:19 +0200

    set dlls to native via regedit

--- install-mf.sh
+++ install-mf.sh
@@ -4,14 +4,27 @@
 
 set -e
 
+overrideDll() {
+  wine reg add 'HKEY_CURRENT_USER\Software\Wine\DllOverrides' /v $1 /d native /f >/dev/null 2>&1
+  if [ $? -ne 0 ]; then
+    echo -e "Failed to add override for $1"
+    exit 1
+  fi
+}
+
 scriptdir=$(dirname "$0")
 cd "$scriptdir"
 
 cp -v syswow64/* "$WINEPREFIX/drive_c/windows/syswow64"
 cp -v system32/* "$WINEPREFIX/drive_c/windows/system32"
 
-echo "Set to native in Libraries: mf, mferror, mfplat, mfreadwrite, msmpeg2adec, msmpeg2vdec, sqmapi"
-WINEDEBUG="-all" winecfg
+overrideDll "mf"
+overrideDll "mferror"
+overrideDll "mfplat"
+overrideDll "mfreadwrite"
+overrideDll "msmpeg2adec"
+overrideDll "msmpeg2vdec"
+overrideDll "sqmapi"
 
 wine start regedit.exe mf.reg
 wine start regedit.exe wmf.reg
